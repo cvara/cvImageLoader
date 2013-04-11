b104992206a7e096ee8dee3370621d86d73f8013
Christoforos Varakliotis <c.varakliotis@gmail.com>
2013-04-11 16:06:32 +0300

    the ability to ignore already loaded background images is exposed as a plugin option

--- jquery.cvImageLoader.js
+++ jquery.cvImageLoader.js
@@ -1,7 +1,8 @@
 /**
  * @name		jQuery cvImageLoader Plugin
  * @author		Christopher Varakliotis
- * @version		1.0
+ * @version		1.0.1
+ * @url			https://github.com/TheT0dd/cvImageLoader/blob/master/jquery.cvImageLoader.js
  * @licence		CC BY (http://creativecommons.org/licenses/by/3.0/)
  * 
  */
@@ -12,8 +13,12 @@
 		
 	    var opts = $.extend({}, $.fn.cvImageLoader.defaults, options);
 	         
-		// Find image elements inside the given context, excluding those loaded already
-		var imageElements = $(this).find('[style*="background-image"]').not('.cv-loaded');
+		// Find image elements inside the given context
+		var imageElements = $(this).find('[style*="background-image"]');
+		
+		if(opts.clever) {
+			imageElements = imageElements.not('.cv-loaded');
+		}
 		
 		if(opts.filter) {
 			imageElements = imageElements.filter(opts.filter);
@@ -27,19 +32,26 @@
 		
 		// If images exist inside the context
 		if(imageCount > 0) {
+			
 			var imagesLoaded = 0;
+			
 			// ...force browser to fetch them
 			imageElements.each(function() {
+				
 				var currentImageElement = $(this);
 				var src = currentImageElement.css('background-image').replace(/(^url\()|(\)$|[\"\'])/g, '');
+				
 				// For each image loaded
 				$('<img/>').attr('src', src).load(function() {
+					
 					// ...mark it with the 'cv-loaded' class
 					currentImageElement.addClass('cv-loaded');
+					
 					// If graceful presentation is on, show it with a fade in effect
 					if(opts.graceful) {
 						currentImageElement.css({'visibility':'visible'}).animate({'opacity':'1'}, opts.showDelay);
 					}
+					
 					// If all images have been fetched
 					if( ++imagesLoaded == imageCount && $.isFunction(opts.ready)) {
 						// ...call given callback function
@@ -50,6 +62,7 @@
 		}
 		// If no images exist inside the context
 		else if($.isFunction(opts.ready)) {
+			
 			// ...call given callback function
 			opts.ready.call();			
 		}	    
@@ -59,7 +72,8 @@
 		graceful: true,
 		showDelay: 900,
 		filter: false,
-		ready: null
+		ready: null,
+		clever: true
 	};	
 	
 })(jQuery);
